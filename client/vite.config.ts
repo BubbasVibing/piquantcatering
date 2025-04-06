6d692564e87749356908e247a995c850cc6062cd
Yassine M <yassinem@n3-31-129.dhcp.drexel.edu>
2025-04-05 21:12:19 -0400

    Fix SPA routing with additional fallback configurations

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
   },
   server: {
-    port: 3000
+    port: 3000,
+    strictPort: true
   },
   preview: {
     port: 8080
